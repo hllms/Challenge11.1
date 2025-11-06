30e2200b1f567051b6c8cb764ac6fe5f4fdfa84a
Jason Hellums <jason.hellums@outlook.com>
2025-11-06 13:29:58 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Challenge11._1
+{
+    internal class Program
+    {
+        public static void InsertionSort(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int key = nums[i];
+                int j = i - 1;
+                while (j >= 0 && nums[j] > key)
+                {
+                    nums[j + 1] = nums[j];
+                    j--;
+                }
+                nums[j + 1] = key;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 0, 1  };
+            InsertionSort(numbers);
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
